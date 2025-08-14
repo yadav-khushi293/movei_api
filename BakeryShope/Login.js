7eee7b0867d88b5fac19e3d57d757ff8006a6832
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-14 13:15:25 +0530

    xyz

--- BakeryShope/Login.js
+++ BakeryShope/Login.js
@@ -8,6 +8,49 @@ async function loginForm(e) {
 
     const email = document.querySelector("#userEmail").value.trim();
     const password = document.querySelector("#userPassword").value.trim();
+
+      if(email.indexOf("@")<=0){
+     
+        document.getElementById("message").innerHTML="Invalid Email By @";
+        return false;
+    }
+    else if(email.charAt(email.length-4)!=="." && email.charAt(email.length-3)!=="."){
+      document.getElementById("message").innerHTML="Invalid Email by . com/.in domain";
+      return false;
+    }
+
+
+    if(password!==null){
+      if(password.trim()===""){
+        console.log('pls enter the value...')
+      }else if(password.length>8 && password.length>20){
+         console.log("pls enter the correct lenght...");
+      }else{
+        const hasUpperCase = /[A-Z]/.test(password);
+         const hasLowerCase = /[A-Z]/.test(password);
+          const hasNumber = /[0-9]/.test(password);
+          const hasSpecialChar = /[!#$%&* (),.?:{}|<>/.]/.test(password); 
+
+          if(!hasUpperCase){
+            console.log("Password must inclued at least one Upper Letter");
+          }else if(!hasLowerCase)
+            {
+            console.log("Password must inclued at least one Lower Letter");
+          }  else if(!hasNumber)
+            {
+             console.log("Password must inclued at least one Number");
+          }else if(!hasSpecialChar)
+          {
+           console.log("Password must inclued at least hasSpecialChar")
+          }
+          else{
+            console.log("Password is valid...");
+          }
+      }
+    }
+
+
+
     let formObject = {
         email,
         password
--- BakeryShope/sighup.js
+++ BakeryShope/sighup.js
@@ -9,6 +9,46 @@ async function formData(e) {
     const email = document.querySelector("#userEmail").value.trim();
     const password = document.querySelector("#userPassword").value.trim();
 
+      if(email.indexOf("@")<=0){
+     
+        document.getElementById("message").innerHTML="Invalid Email By @";
+        return false;
+    }
+    else if(email.charAt(email.length-4)!=="." && email.charAt(email.length-3)!=="."){
+      document.getElementById("message").innerHTML="Invalid Email by . com/.in domain";
+      return false;
+    }
+
+
+    if(pass!==null){
+      if(pass.trim()===""){
+        console.log('pls enter the value...')
+      }else if(pass.length>8 && pass.length>20){
+         console.log("pls enter the correct lenght...");
+      }else{
+        const hasUpperCase = /[A-Z]/.test(pass);
+         const hasLowerCase = /[A-Z]/.test(pass);
+          const hasNumber = /[0-9]/.test(pass);
+          const hasSpecialChar = /[!#$%&* (),.?:{}|<>/.]/.test(pass); 
+
+          if(!hasUpperCase){
+            console.log("Password must inclued at least one Upper Letter");
+          }else if(!hasLowerCase)
+            {
+            console.log("Password must inclued at least one Lower Letter");
+          }  else if(!hasNumber)
+            {
+             console.log("Password must inclued at least one Number");
+          }else if(!hasSpecialChar)
+          {
+           console.log("Password must inclued at least hasSpecialChar")
+          }
+          else{
+            console.log("Password is valid...");
+          }
+      }
+    }
+
     let formObject = {
         email,
         password
