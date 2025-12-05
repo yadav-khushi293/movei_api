cc59e0db8e9878b0700cbb554d811e7c3c11e0b2
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-12-05 23:30:15 +0530

    code

---
+++ BakeryShope/Js/todolist.js
@@ -0,0 +1,18 @@
+
+function handleForm(e) {
+  e.preventDefault();
+  let todoName = document.querySelector("#taskInput").value;
+
+  let personData = {
+    id: Date.now(),
+    textTodo: todoName,
+    isEdit: false,
+    isComplete: false,
+  };
+}
+
+const renderTodoList = () => {
+
+    const maindiv_1 =document.crw
+
+}
